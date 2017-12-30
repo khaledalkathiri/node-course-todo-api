7abefba277c3d6993c9a4c533cc15e0b6fcf0bea
khaled Alkathiri <kk-kk20@hotmail.com>
2017-12-31 02:20:50 +0300

    adding update

---
+++ playground/mongodb-update.js
@@ -0,0 +1,27 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err){
+         console.log("Unable to connect to MongoDb");
+    }
+    console.log('Connected to MongoDB server');
+   
+    
+    const myDB = db.db('TodoApp');
+
+
+    myDB.collection('Todos').findOneAndUpdate({
+        _id: new ObjectID('5a48117e67add57128db2c12')
+    },{
+        $set: { completed: false}
+    },{
+        returnOriginal: false
+    })
+    .then((result) =>{
+        console.log(result);
+    }, (err) =>{
+        console.log("Unable to delete data", err);
+    });
+    
+    //db.close();
+});
